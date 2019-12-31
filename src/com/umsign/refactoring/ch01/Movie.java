2784d72c0b9483add1130621a2d6ec045a1149c6
ukh <able.kiho.um@gmail.com>
2019-12-31 22:54:54 +0900

    리펙토링, 첫 번째 예제(조건문을 다형성으로 바꾸기)

--- src/com/umsign/refactoring/ch01/Movie.java
+++ src/com/umsign/refactoring/ch01/Movie.java
@@ -6,15 +6,15 @@ public class Movie {
     public static final int NEW_RELEASE = 1;
 
     private String title;
-    private int priceCode;
+    private Price price;
 
     public Movie(String title, int priceCode) {
         this.title = title;
-        this.priceCode = priceCode;
+        setPriceCode(priceCode);
     }
 
     public int getPriceCode() {
-        return priceCode;
+        return price.getPriceCode();
     }
 
     public String getTitle() {
@@ -22,6 +22,26 @@ public class Movie {
     }
 
     public void setPriceCode(int priceCode) {
-        this.priceCode = priceCode;
+        switch (priceCode) {
+            case REGULAR:
+                price = new RegularPrice();
+                break;
+            case CHILDREN:
+                price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                price = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("Incorrect Price Code");
+        }
+    }
+
+    double getCharge(int daysRented) {
+        return price.getCharge(daysRented);
+    }
+
+    int getFrequentRenterPoints(int daysRented) {
+        return price.getFrequentRenterPoints(daysRented);
     }
 }
